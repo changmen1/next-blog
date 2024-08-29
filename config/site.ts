1700f243813263e390c69636698e8ea8a2a3d59c
zxl17339811909@163.com <zxl17339811909>
2024-08-29 16:20:14 +0800

    headerfooterthems

---
+++ config/site.ts
@@ -0,0 +1,14 @@
+/**本地静态资源配置 */
+export const siteConfig = {
+    name: "ZxlBlog",
+    url: "https://example.com",
+    description: "Nextjs 14 blog using velite, tailwind and shadcn",
+    author: "ZxlCoding",
+    links: {
+      twitter: "https://github.com/changmen1",
+      github: "https://github.com/changmen1",
+      personalSite: "https://github.com/changmen1",
+    },
+  };
+  
+  export type SiteConfig = typeof siteConfig;
