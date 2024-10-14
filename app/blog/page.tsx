492a4306030177ef6f74e2ae4b630a5ca3b2b633
zxl17339811909@163.com <zxl17339811909>
2024-10-14 16:47:23 +0800

    deletestyles

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -24,8 +24,7 @@ export default async function BlogPage({ searchParams }: BlogPageProps) {
     <div className="container max-w-4xl py-6 lg:py-10">
       <div className="flex flex-col items-start gap-4 md:flex-row md:justify-between md:gap-8">
         <div className="flex-1 space-y-4">
-          {/* <h1 className="inline-block font-black text-4xl lg:text-5xl">Blog</h1> */}
-          <h1>Blog</h1>
+          <h1 className="inline-block font-black text-4xl lg:text-5xl">Blog</h1>
           <p className="text-xl text-muted-foreground">
             筹划中,暂时没想好,放只我家妹妹吧😽.
           </p>
